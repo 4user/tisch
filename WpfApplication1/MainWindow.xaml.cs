aaffaf94c450b284f8398cad75755779abcd9330
4user <sonderzeichenu@yahoo.de>
2014-01-03 16:48:43 +0100

    Init Tisch

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void start(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown(0);
+        }
+        private void signin(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void HelpCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+            e.Handled = true;
+        }
+
+        private void HelpExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Hey, I'm some help.");
+            e.Handled = true;
+        }
+
+        private void Pos2_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+    }
+}
